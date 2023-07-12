3dc6f4041a484d580d886a689f6a79c5b2639bc1
Israel <itepayotl@yahoo.com>
2023-07-12 11:24:14 -0700

    complete job

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(drivers, string){
+
+    const riders = drivers.filter(function (driver) {return string.toLowerCase() === driver.toLowerCase()});
+  
+    return riders;
+  }
+function fuzzyMatch(drivers, string) {
+  let match = string.length
+  const allDrivers = drivers.filter(function(driver) {return string.toLowerCase() === driver.toLowerCase().substring(0,match) 
+  })
+return allDrivers
+  }
+function matchName(drivers, name) {
+  const filtered = drivers.filter(function(driver){
+    return driver.name.toLowerCase() === name.toLowerCase()
+  })
+  return filtered 
+}
+
+
+
+//{
+  //return drivers.filter((driver) => {
+    //if (driver.name.toLowerCase() === name.toLowerCase()) {
+      //return true;
+   // }
+  //})
+//}
